419761a6d73ecbf312f72585629fb0d2d11eefc4
Roshan <roshanmaurya729@gmail.com>
2022-09-24 05:53:08 +0530

    Prepared for API integration

--- components/home/MapComponent.js
+++ components/home/MapComponent.js
@@ -1,10 +1,24 @@
 import { Box } from "@mui/material";
-import Image from "next/image";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+
+// import Image from "next/image";
+
+const center = { lat: 28.53999198480272, lng: 77.16577169607818 };
 
 const MapComponent = () => {
+
+    const { isLoaded } = useJsApiLoader({
+        googleMapsApiKey: process.env.MAPS_API_KEY
+    });
+
+    if(!isLoaded) return <div>Loading...</div>
+
     return (
         <Box>
-            <Image src='/images/card.png' alt='mapImage' height={511} width={560} />
+            {/* <Image src='/images/card.png' alt='mapImage' height={511} width={560} /> */}
+            <GoogleMap center={center} zoom={12} mapContainerStyle={{width: '100%', height: '100%'}}>
+
+            </GoogleMap>
         </Box>
     );
 };
